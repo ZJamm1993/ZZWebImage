ee28c919d2a7521fa97b9425ddd5452d6e44a0a2
zhangjingjian <zhangjingjian@dabby.cn>
2018-07-13 14:36:01 +0800

    mv Lib to outside

--- ZZWebImage/ZZHttpTool.h
+++ ZZWebImage/ZZHttpTool.h
@@ -12,7 +12,7 @@
 
 +(void)get:(NSString *)url params:(NSDictionary *)params usingCache:(BOOL)isCache success:(void (^)(NSDictionary *dict))success failure:(void (^)(NSError *err))failure;
 
-+(void)post:(NSString *)url params:(NSDictionary *)params success:(void(^)(NSDictionary* responseObject))success failure:(void(^)(NSError *error))failure;
+//+(void)post:(NSString *)url params:(NSDictionary *)params success:(void(^)(NSDictionary* responseObject))success failure:(void(^)(NSError *error))failure;
 
 +(void)requestMethod:(NSString*)method url:(NSString *)url params:(NSDictionary *)params usingCache:(BOOL)isCache success:(void (^)(NSDictionary * dict))success failure:(void (^)(NSError *err))failure;
 
